7ec9914bd36bb69f25ace5f56c6370d37b9589f9
Amartya Mondal <hayyoulistentome@gmail.com>
2019-03-19 09:26:50 +0530

    Added enquiry feature

--- contacts/urls.py
+++ contacts/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from . import views
 
-urlpatterns=[
-    path('contact',views.contact, name='contact')
+urlpatterns = [
+    path('contact', views.contact, name='contact')
 ]
